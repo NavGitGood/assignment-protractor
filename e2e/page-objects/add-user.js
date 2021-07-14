ee6e85548ca865c32b3614dc8369fcd68fb1dec6
Navneet Gupta <nnav4u@gmail.com>
2021-07-14 23:53:33 +0530

    more scenarios

--- e2e/page-objects/add-user.js
+++ e2e/page-objects/add-user.js
@@ -5,8 +5,8 @@ const firstNameTxt = $("input[name=FirstName]");
 const lastNameTxt = $("input[name=LastName]");
 const userNameTxt = $("input[name=UserName]");
 const passwordTxt = $("input[name=Password]");
-const aaaCompanyRdBtn = $("input[name=15]");
-const bbbCompanyRdBtn = $("input[name=16]");
+const aaaCompanyRdBtn = $("input[value='15']");
+const bbbCompanyRdBtn = $("input[value='16']");
 const roleSelect = $("select[name=RoleId]");
 const emailTxt = $("input[name=Email]");
 const phoneTxt = $("input[name=Mobilephone]");
@@ -17,26 +17,46 @@ async function openAddUserDialog() {
     await addUserBtn.click();
 }
 
-async function fillAddUserData() {
-    await firstNameTxt.sendKeys("FirstName");
-    browser.sleep(2000);
-    await lastNameTxt.sendKeys("FirstName");
-    browser.sleep(2000);
-    await userNameTxt.sendKeys("FirstName");
-    browser.sleep(2000);
-    await passwordTxt.sendKeys("FirstName");
-    browser.sleep(2000);
-    await emailTxt.sendKeys("FirstName");
-    browser.sleep(2000);
-    await phoneTxt.sendKeys("FirstName");
-    browser.sleep(2000);
+async function fillAddUserData(userData) {
+    await firstNameTxt.sendKeys(userData.firstName);
+    // browser.sleep(2000);
+    await lastNameTxt.sendKeys(userData.lastName);
+    // browser.sleep(2000);
+    await userNameTxt.sendKeys(userData.userName);
+    // browser.sleep(2000);
+    await passwordTxt.sendKeys(userData.password);
+    // browser.sleep(2000);
+    await phoneTxt.sendKeys(userData.phone);
+    // browser.sleep(2000);
+    await selectRole(userData.role);
+    // browser.sleep(2000);
+    await emailTxt.sendKeys(userData.email);
+    // browser.sleep(2000);
+    console.log("-------------", userData.customer);
+    await selectCustomer(userData.customer); // this is a bug, never displayed on table
+    // browser.sleep(2000);
+}
+
+async function selectRole(role) {
+    // await roleSelect.click();
+    // browser.sleep(2000);
+    await roleSelect.$$("option").filter(opt => opt.getText()
+    .then(innerText => innerText === role)
+    ).get(0).click();
+    // browser.sleep(2000);
+}
+
+async function selectCustomer(customer) {
+    if (customer === "AAA") await aaaCompanyRdBtn.click();
+    else if (customer === "BBB") await bbbCompanyRdBtn.click();
 }
 
 async function addUser() {
-
+    await saveBtn.click();
 }
 
 module.exports = {
     openAddUserDialog,
-    fillAddUserData
+    fillAddUserData,
+    addUser
 }
---
+++ e2e/page-objects/user-data-table.js
@@ -0,0 +1,51 @@
+const { $, browser } = require("protractor");
+
+const table = $("table");
+const dataRow = table.$$("tbody > tr");
+const headerRow = table.$("thead > tr.smart-table-header-row");
+const tableSearch = $(".smart-table-global-search > input");
+const deletePopupBtn = $(".modal-footer > .btn-primary");
+
+async function getColumnIdFromHeaderName(headerName) {
+    const headerData = await headerRow.$$("th").map(async column => await column.getText());
+    return headerData.findIndex(header => header === headerName);
+}
+
+async function getTableRowByColumnValue(headerName, columnValue) {
+    const index = await getColumnIdFromHeaderName(headerName);
+    return dataRow.filter(
+        row => row.$$("td").get(index).getText()
+            .then(innerText => innerText === columnValue)
+    ).get(0);
+}
+
+async function readARow(row) {
+    return row.$$("td").map(async column => await column.getText())
+}
+
+async function getDataForARow(headerName, columnValue) {
+    const row = await getTableRowByColumnValue(headerName, columnValue);
+    const data = await readARow(row);
+    return data;
+}
+
+async function searchUser(valueToBeSearched) {
+    tableSearch.sendKeys(valueToBeSearched);
+}
+
+async function getRowCount() {
+    return dataRow.count();
+}
+
+async function deleteFirstRow() {
+    await dataRow.get(0).$("td .icon-remove").click();
+    await browser.sleep(1000);
+    await deletePopupBtn.click();
+}
+
+module.exports = {
+    getDataForARow,
+    searchUser,
+    getRowCount,
+    deleteFirstRow
+}
